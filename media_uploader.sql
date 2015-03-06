27bdb86d3a51f0ead6ceb44602f2a93213a8337f
Damiano Ciarla <damiano.ciarla@gmail.com>
2015-03-06 20:34:05 +0100

    Aggiornamento tabelle

--- media_uploader.sql
+++ media_uploader.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Mar 05, 2015 alle 10:55
+-- Generation Time: Mar 06, 2015 alle 20:33
 -- Versione del server: 5.5.38
 -- PHP Version: 5.5.18
 
@@ -24,7 +24,7 @@ CREATE TABLE `media` (
 `id` int(11) NOT NULL,
   `context` varchar(140) COLLATE utf8_unicode_ci NOT NULL,
   `path` varchar(255) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -63,6 +63,18 @@ CREATE TABLE `media_image` (
   `size` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `media_pdf`
+--
+
+CREATE TABLE `media_pdf` (
+  `media_id` int(11) NOT NULL,
+  `size` bigint(20) DEFAULT NULL,
+  `total_page_number` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 --
 -- Indexes for dumped tables
 --
@@ -86,6 +98,12 @@ ALTER TABLE `media_image`
  ADD PRIMARY KEY (`media_id`);
 
 --
+-- Indexes for table `media_pdf`
+--
+ALTER TABLE `media_pdf`
+ ADD PRIMARY KEY (`media_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -93,7 +111,7 @@ ALTER TABLE `media_image`
 -- AUTO_INCREMENT for table `media`
 --
 ALTER TABLE `media`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=28;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=36;
 --
 -- Limiti per le tabelle scaricate
 --
