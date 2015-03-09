41f273b22edf186bb16f9d2d4998f171d2bdb5eb
Lorenzo Ferrara Junior <lorenzoferrarajr@gmail.com>
2015-03-09 15:16:41 +0100

    ContextLocator extends Zend Service Manager

--- src/UEC/MediaUploader/Core/ContextLocator/ContextLocator.php
+++ src/UEC/MediaUploader/Core/ContextLocator/ContextLocator.php
@@ -3,8 +3,9 @@
 namespace UEC\MediaUploader\Core\ContextLocator;
 
 use UEC\MediaUploader\Core\Configuration\TypeConfigurationInterface;
+use Zend\ServiceManager;
 
-class ContextLocator implements ContextLocatorInterface
+class ContextLocator extends ServiceManager\ServiceManager implements ContextLocatorInterface
 {
     /**
      * Associative array where the key is the name of the context and the value is an instance of TypeConfigurationInterface
@@ -13,32 +14,9 @@ class ContextLocator implements ContextLocatorInterface
      */
     protected $contexts;
 
-    function __construct(array $contexts)
-    {
-        $this->contexts = $contexts;
-    }
-
     public function set($context, TypeConfigurationInterface $typeConfiguration)
     {
         $this->contexts[$context] = $typeConfiguration;
         return $this;
-    }
-
-    public function has($context)
-    {
-        return array_key_exists($context, $this->contexts);
-    }
-
-    public function get($context)
-    {
-        if (!is_scalar($context) && !is_string($context)) {
-            throw new \UnexpectedValueException('The value must be a string');
-        }
-
-        if (!$this->has($context)) {
-            throw new \OutOfBoundsException(sprintf('The context "%s" is not present in the configuration', $context));
-        }
-
-        return $this->contexts[$context];
     }
 }
